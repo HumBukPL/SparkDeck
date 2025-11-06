ff8d2ef781fe343ae3886594bca70947d8229e38
Karol Kołodziejczyk <k.kolodziejczykk01@gmail.com>
2025-11-06 15:31:24 +0100

    feat: implement SparkGrid component for displaying sparks with data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,9 @@
-import Card from '@/components/Card'
-
-// Temporary data for demonstration purposes, later to be replaced with real data fetching
-const tempSparkData = [
-  {
-    title: 'OnePiece',
-    featuredWords: ['react', 'nextjs', 'tailwind'],
-    image: 'onepiece.webp',
-  },
-  {
-    title: 'Naruto Shippuden',
-    featuredWords: ['javascript', 'typescript', 'css'],
-    image: 'naruto.jpg',
-  },
-  {
-    title: 'Test of a very long title that should be truncated',
-    featuredWords: ['html', 'webdev', 'frontend'],
-  },
-  {
-    title: 'Spark 4',
-    featuredWords: ['nodejs', 'express', 'backend'],
-  },
-  {
-    title: 'Spark 5',
-    featuredWords: ['database', 'sql', 'nosql'],
-  },
-]
+import SparkGrid from '@/components/SparkGrid'
 
 export default function Home() {
   return (
     <>
-      {/* TODO: Move it to a separate component  */}
-      <div className="grid gap-3 sm:grid-cols-3 lg:grid-cols-4">
-        {tempSparkData.map((spark) => (
-          <Card
-            key={spark.title}
-            title={spark.title}
-            featuredWords={spark.featuredWords}
-            image={spark.image}
-          />
-        ))}
-      </div>
+      <SparkGrid />
     </>
   )
 }
---
+++ components/SparkGrid.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import Card from '@/components/Card'
+import { Spark } from '@/types/spark'
+import { getSparksFromLocal } from '@/utils/addSparkUtils'
+import { useEffect, useState } from 'react'
+
+export default function SparkGrid() {
+  const [sparks, setSparks] = useState<Array<Spark>>([])
+  useEffect(() => {
+    const featchSparks = async () => {
+      const storedSparks = getSparksFromLocal()
+      setSparks(storedSparks)
+    }
+    featchSparks()
+  }, [])
+
+  return (
+    <div className="grid gap-3 sm:grid-cols-3 lg:grid-cols-4">
+      {sparks.map((spark) => (
+        <Card
+          key={spark.title}
+          title={spark.title}
+          featuredWords={spark.featuredWords}
+          image={spark.image}
+        />
+      ))}
+    </div>
+  )
+}
