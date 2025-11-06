9ba009952980781af8044170eb0ef30894c40b19
Karol Kołodziejczyk <k.kolodziejczykk01@gmail.com>
2025-11-06 15:16:49 +0100

    feat: enhance AddSparkForm with validation and input components; add Spark type and local storage utility functions

--- components/AddSparkForm.tsx
+++ components/AddSparkForm.tsx
@@ -2,27 +2,98 @@
 
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
-import { z } from 'zod'
-import { Form } from './ui/form'
-import { addSparkFormSchema } from '@/shema/addSparkFormSchema'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form'
+import { addSparkFormSchema } from '@/schema/addSparkFormSchema'
+import type { AddSparkFormSchema } from '@/schema/addSparkFormSchema'
+import { Input } from './ui/input'
+import { TagsInput } from './TagsInput'
+import { ImageInput } from './ImageInput'
+import { Button } from './ui/button'
+import { saveSparkToLocal } from '@/utils/addSparkUtils'
+import { useRouter } from 'next/navigation'
 
 export default function AddSparkForm() {
-  const addSparkForm = useForm<z.infer<typeof addSparkFormSchema>>({
+  const history = useRouter()
+  const addSparkForm = useForm<AddSparkFormSchema>({
     resolver: zodResolver(addSparkFormSchema),
     defaultValues: {
       title: '',
-      featuredWords: [''],
+      featuredWords: [],
       image: '',
     },
   })
 
-  const onSubmit = (data: z.infer<typeof addSparkFormSchema>) => {
-    console.log(data)
+  //TODO: replace with DB logic later
+  const onSubmit = (data: AddSparkFormSchema) => {
+    console.log('Submitted data:', data)
+    saveSparkToLocal(data)
+    history.push('/')
+    addSparkForm.reset()
   }
 
   return (
-    <Form {...addSparkForm}>
-      <form onSubmit={addSparkForm.handleSubmit(onSubmit)} className="space-y-4"></form>
-    </Form>
+    <div className="mx-auto w-full max-w-md md:max-w-lg">
+      <Form {...addSparkForm}>
+        <form onSubmit={addSparkForm.handleSubmit(onSubmit)}>
+          <FormField
+            control={addSparkForm.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="My Spark" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={addSparkForm.control}
+            name="featuredWords"
+            render={({ field }) => (
+              <FormItem className="mt-6">
+                <FormLabel>Featured Words</FormLabel>
+                <FormControl>
+                  <TagsInput value={field.value} onChange={field.onChange} />
+                </FormControl>
+                <FormDescription>
+                  Provide featured words for your Spark. Try to describe it with three words.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={addSparkForm.control}
+            name="image"
+            render={({ field }) => (
+              <FormItem className="mt-6">
+                <FormLabel>Picture of your Spark</FormLabel>
+                <FormControl>
+                  <ImageInput onChange={field.onChange} />
+                </FormControl>
+                <FormDescription>Provide an image for your Spark (optional).</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="my-8 flex justify-end gap-3">
+            <Button variant="outline">Clear</Button>
+            <Button disabled={!addSparkForm.formState.isValid} type="submit">
+              Create
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
   )
 }
---
+++ types/spark.ts
@@ -0,0 +1,5 @@
+export type Spark = {
+  title: string
+  featuredWords: string[]
+  image?: string
+}
