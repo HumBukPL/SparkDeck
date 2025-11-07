9b506fbc6847002de336c7a620fec5933e6baa2e
Karol Kołodziejczyk <k.kolodziejczykk01@gmail.com>
2025-11-07 14:15:19 +0100

    feat: integrate usehooks-ts for local storage management and remove legacy spark utility functions

--- components/SparkGrid.tsx
+++ components/SparkGrid.tsx
@@ -4,16 +4,20 @@ import { Card } from '@/components/Card'
 import { Spark } from '@/types/spark'
 import { getSparksFromLocal } from '@/utils/sparkUtils'
 import { useEffect, useState } from 'react'
+import { useLocalStorage } from 'usehooks-ts'
 
 export default function SparkGrid() {
-  const [sparks, setSparks] = useState<Array<Spark>>([])
-  useEffect(() => {
-    const featchSparks = async () => {
-      const storedSparks = getSparksFromLocal()
-      setSparks(storedSparks)
-    }
-    featchSparks()
-  }, [])
+  const [sparks] = useLocalStorage<Array<Spark>>('sparks', [])
+
+
+  // const [sparks, setSparks] = useState<Array<Spark>>([])
+  // useEffect(() => {
+  //   const featchSparks = async () => {
+  //     const storedSparks = getSparksFromLocal()
+  //     setSparks(storedSparks)
+  //   }
+  //   featchSparks()
+  // }, [])
 
   return (
     <div className="grid gap-3 sm:grid-cols-3 lg:grid-cols-4">
